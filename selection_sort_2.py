fc74170ab7d59690ee1867e826f9b3a85901283f
Om Wayal <93111215+oMiirOxx@users.noreply.github.com>
2024-11-13 06:00:24 +0530

    Update selection_sort_2.py

--- selection_sort_2.py
+++ selection_sort_2.py
@@ -1,13 +1,14 @@
-#def selectionSort(arr):
-#    for i in range(len(arr)):
-#        min = float('-inf')
-#        for j in range(i + 1, len(arr)):
-#            if arr[i] > arr[j]:
-#                arr[i],arr[j] = arr[j], arr[i]
-#    return arr
+'''
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min = float('-inf')
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i],arr[j] = arr[j], arr[i]
+    return arr
     
-#print(selectionSort([89,56,45,34,65,76]))
-
+print(selectionSort([89,56,45,34,65,76]))
+'''
 def selectionSort(arr):
     for i in range(len(arr)):
         # Assume the current element is the minimum
@@ -20,7 +21,7 @@ def selectionSort(arr):
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
-#print(selectionSort([89,56,45,34,65,76]))                      for inbuilt input
+#print(selectionSort([89,56,45,34,65,76]))                      #for inbuilt input
 
 # Take input from the user
 n = int(input("Enter the number of elements: "))
